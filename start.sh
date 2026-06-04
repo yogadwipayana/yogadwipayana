7338d1fa7a21ddf60318ae24867bfc3cdfbc3028
yogadwipayana <yogadwipayana2006@gmail.com>
2026-06-04 09:45:13 +0800

    Add PM2 deployment script

---
+++ start.sh
@@ -0,0 +1,11 @@
+pm2 stop yogadwipayana
+pm2 delete yogadwipayana
+
+cd /yogadwipayana
+git pull
+
+npm install
+npm run build
+PORT=3000 pm2 start npm --name "yogadwipayana" -- run start
+
+pm2 save
